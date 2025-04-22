10832cbfc32f1bdfad1afdfe12d551e026fe5258
Nico <nico@thebeachhut.dev>
2025-04-22 11:54:42 -0400

    Changed title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,7 +25,7 @@
   <div class="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div class="sm:mx-auto sm:w-full sm:max-w-md">
         <img class="mx-auto h-10 w-auto" src="/mistral-logo.png" alt="Your Company">
-        <h2 class="mt-6 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Answering machine</h2>
+        <h2 class="mt-6 text-center text-2xl/9 font-bold tracking-tight text-gray-900">What are the rules of ... ?</h2>
       </div>
     
       <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
